a741ee0b56474bfa8bda0b9f0430abb8d5f179c2
Felix Meyer-Wolters <felix@egoversum.com>
2016-10-27 15:40:51 +0200

    Change log message for blocking other instances from "info" to "debug"

--- vhpi/main.py
+++ vhpi/main.py
@@ -36,7 +36,7 @@ def check_lock(lockfile):
         lockfile = open(lockfile, 'w')
         fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
     except BlockingIOError:
-        log.info('    Info: Another instance of this Script was executed, '
+        log.debug('    Info: Another instance of this Script was executed, '
                  'but it was blocked successfully. ' +
                  time.strftime(S.timestamp_format))
         sys.exit()
